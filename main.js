02f17d3dc5b410bf863a7b6257d1da255c2c0a6a
Farhod Gofurov <fg325609@gmail.com>
2026-04-15 15:00:01 +0500

    kjffkf

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const wrapper = document.getElementById("container");
 
-fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers");
+fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
+.then(Response => Response.json())
+.then(data =>{
+    render(data)
+})
+.catch(error=> console.error("xatolik yuz berdi"));
