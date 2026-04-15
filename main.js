1db10d9be77cb3d03b419fd349f53057fa1772c6
Farhod Gofurov <fg325609@gmail.com>
2026-04-15 15:06:32 +0500

    jkffjf

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const wrapper = document.getElementById("container");
 fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
 .then(Response => Response.json())
 .then(data =>{
-    render(data)
-})
-.catch(error=> console.error("xatolik yuz berdi",error));
-function render(users){
-    const cards = users.map(el =>`
-        `)
-}
+    console.log(data);
+    
+    // render(data)
+});
+// .catch(error=> console.error("xatolik yuz berdi",error));
+// function render(users){
+//     const cards = users.map(el =>`
+//         `)
+// }
