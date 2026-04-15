d06e49b7232903bbea50c38a7b1b3b415021a2ea
Farhod Gofurov <fg325609@gmail.com>
2026-04-15 16:26:50 +0500

    jhfff

--- main.js
+++ main.js
@@ -4,34 +4,32 @@ const search = document.getElementById("input");
 fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
   .then(response => response.json())
   .then(data => {
-    render(data); // Dastlab barcha ma'lumotni chiqaradi
-    filterInput(data); // Qidiruv funksiyasini ishga tushiradi
+    render(data); 
+    filterInput(data); 
   })
   .catch(error => console.error("Xatolik yuz berdi:", error));
 
 function render(users) {
   const cards = users.map(el => `
     <div class="card" key="${el.id}">
-        <img src="${el.avatar}" alt="${el.name}" style="width:100px">
+        <img src="${el.avatar}" alt="${el.name}"">
         <p>${el.name}</p>
         <a href="#">${new Date(el.createdAt).toLocaleDateString()}</a>
     </div>
-  `).join(''); // Join ichiga bo'sh satr qo'shish shart
+  `).join('');
 
   wrapper.innerHTML = cards;
 }
 
 function filterInput(data) {
-  // "input" hodisasi har bir harf yozilganda ishlaydi
   search.addEventListener("input", (e) => {
     let inputValue = e.target.value.toLowerCase();
 
     let filteredData = data.filter(item => {
-      // Ismi yoki sanasi mos kelishini tekshiramiz
       return item.name.toLowerCase().includes(inputValue) || 
              item.createdAt.toLowerCase().includes(inputValue);
     });
 
-    render(filteredData); // Faqat qidiruvga moslarini qayta chizadi
+    render(filteredData); 
   });
 }
